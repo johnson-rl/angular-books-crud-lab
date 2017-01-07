55fbdd8581312e31314cd3abb157e90fa0c67cfe
johnson-rl <johnson.rl@gmail.com>
2017-01-06 18:27:14 -0800

    finished basic challenges

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -8,11 +8,53 @@ BooksShowController.$inject=['$routeParams', '$location', '$http'];
 function BooksShowController($routeParams, $location, $http) {
   var vm = this;
   var bookId = $routeParams.id;
-  $http({
-    method: 'GET',
-    url: 'https://super-crud.herokuapp.com/books/'+bookId
+  vm.loadBook = function (){
+    $http({
+      method: 'GET',
+      url: 'https://super-crud.herokuapp.com/books/'+bookId
     }).then(function success(response){
       vm.book = response.data
       console.log(vm.book)
     })
+  }
+
+  vm.loadBook()
+
+  vm.editBook = function(book){
+    console.log(book)
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/'+bookId,
+      data: book
+      // data: {
+      // title: book.title,
+      // author: book.author,
+      // image: book.image,
+      // releaseDate: book.releaseDate,
+      // }
+    }).then(function editSuccess(res){
+      console.log(res)
+      vm.book = res.data
+    }, function editFail(res){
+      console.log('failed', res)
+    })
+  }
+
+  vm.deleteBook = function(book){
+    console.log(book)
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/'+bookId,
+    }).then(function editSuccess(res){
+      console.log(res)
+      vm.book = res.data
+      vm.loadLibrary()
+    }, function editFail(res){
+      console.log('failed', res)
+    })
+  }
+
+  vm.loadLibrary = function (){
+    $location.path('/')
+  }
 };
