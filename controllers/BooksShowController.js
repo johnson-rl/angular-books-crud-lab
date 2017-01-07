6788f7f5986ad4b81e8c18170a5d4c84f8afa64a
johnson-rl <johnson.rl@gmail.com>
2017-01-06 16:14:24 -0800

    show is showing

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -2,9 +2,17 @@ angular.module('libraryApp')
   .controller('BooksShowController', BooksShowController);
 
 // controller function and dependency injection
-// $routeParams and $location are required for routing stuff 
+// $routeParams and $location are required for routing stuff
 //   - but you might need to add a dependency
-BooksShowController.$inject=['$routeParams', '$location'];   
-function BooksShowController($routeParams, $location) {
+BooksShowController.$inject=['$routeParams', '$location', '$http'];
+function BooksShowController($routeParams, $location, $http) {
   var vm = this;
+  var bookId = $routeParams.id;
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books/'+bookId
+    }).then(function success(response){
+      vm.book = response.data
+      console.log(vm.book)
+    })
 };
