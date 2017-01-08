22802b65809801dfbeec96cb979ee9437e79221e
johnson-rl <johnson.rl@gmail.com>
2017-01-07 17:15:25 -0800

    all instructions complete

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -7,10 +7,28 @@ BooksIndexController.$inject = ['$http'];
 function BooksIndexController ($http){
   console.log('books!');
   var vm = this;
+  vm.books=[]
+  vm.newBook = {image: 'http://bit.ly/2iQvmZ5'}
   $http({
     method: 'GET',
     url: 'https://super-crud.herokuapp.com/books'
   }).then(function success(response){
     vm.books = response.data.books
+    console.log(vm.books)
   })
+
+  vm.saveBook = function(newBook){
+    console.log("got data: ", newBook)
+    $http({
+      method: 'POST',
+      url: 'https://super-crud.herokuapp.com/books/',
+      data: newBook
+    }).then(function newSuccess(res){
+      console.log("saved:", res)
+      vm.books.push(res.data)
+      vm.newBook = {image: 'http://bit.ly/2iQvmZ5'}
+    }, function editFail(res){
+      console.log('failed', res)
+    })
+  }
 }
